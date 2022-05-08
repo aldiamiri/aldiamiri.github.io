c74801095ab048d928fbdac05bb4b7302754b26e
aldiamiri <aldiamiri@gmail.com>
2022-05-08 23:17:07 +0800

    First Commit to Main

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require ('express')
+const app = express ()
+
+app.set ('view engine', 'ejs')
+app.use (express.static('public'))
+app.use (express.json())
+
+app.get ('/', (req,res) => {
+    res.render('index')
+})
+
+app.listen (8000, () => {
+    console.log ('server is running...')
+})
